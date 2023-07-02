7c2b44a3cd5f3cba943af422df4b37bf1ba22e88
Prabhudev Desai <116657791+prabhu797@users.noreply.github.com>
2023-07-02 13:23:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+let a = 1;
+let score = 0;
+let hit = false;
+
+let ran = 1;
+let idInter = 0;
+let img = document.getElementById("num" + ran).firstElementChild;
+function lighting(speed) {
+  idInter = setInterval(function() {
+    console.log("Hello");
+    img = document.getElementById("num" + ran).firstElementChild;
+    img.src = 'Images/RealHole.png';
+    do {
+      newRan = Math.floor(Math.random() * 9) + 1;
+    }while(ran == newRan);
+    ran = newRan;
+    hit = false;
+    img = document.getElementById("num" + ran).firstElementChild;
+    img.src = 'Images/Hole.png';
+  }, speed);
+}
+
+function timeBro(speed) {
+  lighting(speed);
+  let timerId = setInterval(function timer() {
+    document.getElementById("printtime").innerHTML = "Time : " + a + " sec";
+    a++;
+    speed = Number.parseInt(speed);
+    if (1 == (a % 10) && a != 1) {
+      console.log("Hello1");
+      clearInterval(idInter);
+      img = document.getElementById("num" + ran).firstElementChild;
+      img.src = 'Images/RealHole.png';
+      speed = speed - 110;
+      console.log("Hello2");
+      console.log(speed);
+      lighting(speed);
+    }
+    if (a > 60) {
+      clearInterval(idInter);
+      img = document.getElementById("num" + ran).firstElementChild;
+      img.src = 'Images/RealHole.png';
+      ran = 1;
+      a = 1;
+      score = 0;
+      c = 1;
+      clearInterval(timerId);
+    }
+  }, 1000);
+}
+
+function clicked(num) {
+  if (ran == num && hit == false) {
+    img = document.getElementById("num" + ran).firstElementChild;
+    img.src = 'Images/hit.png';
+    score++;
+    hit = true;
+    document.getElementById("printmsg").innerHTML = "Score : " + score;
+  }
+}
+
+let c = 1;
+function startGame() {
+  let dropdown = document.getElementById("difficulty");
+  let speed = dropdown.value;
+  if (c == 1) {
+    c++;
+    timeBro(speed);
+    document.getElementById("printmsg").innerHTML = "Score : " + score;
+    // lighting(speed);
+  }
+}
+
+
